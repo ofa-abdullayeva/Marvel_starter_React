bba7e843f7a9f9abe9be596c98ed56404ba13eb2
ofa-abdullayeva <abdullayevaofa25@gmail.com>
2022-05-13 10:35:42 +0400

    React Router pages

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,16 +1,16 @@
 import './appHeader.scss';
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 
 const AppHeader = () => {
     return (
         <header className="app__header">
             <h1 className="app__title">
-                <Link to="https://dim.gov.az/">
-                    <a href="#https://dim.gov.az/">
+                
+                    <a href="https://dim.gov.az/">
                         <span>Marvel</span> information portal
                     </a>
-                </Link>
+              
 
             </h1>
             <nav className="app__menu">
